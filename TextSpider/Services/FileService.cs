7da848f8c04858f360c8ad7ac543ff834b01495f
Erik-Z <erikzhou7@gmail.com>
2023-07-16 22:27:56 -0400

    Refactored file method in to file service

---
+++ TextSpider/Services/FileService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using WinRT;
+
+namespace TextSpider.Services
+{
+    internal class FileService
+    {
+        public async Task<StorageFile> PickSingleFileAsync(IntPtr hwnd)
+        {
+            var filePicker = new FileOpenPicker();
+            var initializeWithWindow = filePicker.As<IInitializeWithWindow>();
+            initializeWithWindow.Initialize(hwnd);
+            filePicker.FileTypeFilter.Add("*");
+            filePicker.FileTypeFilter.Add(".txt");
+            filePicker.ViewMode = PickerViewMode.List;
+            filePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+
+            return await filePicker.PickSingleFileAsync();
+        }
+
+        public async Task<StorageFolder> PickSingleFolderAsync(IntPtr hwnd)
+        {
+            var folderPicker = new FolderPicker();
+            var initializeWithWindow = folderPicker.As<IInitializeWithWindow>();
+            initializeWithWindow.Initialize(hwnd);
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+
+            return await folderPicker.PickSingleFolderAsync();
+        }
+    }
+}
